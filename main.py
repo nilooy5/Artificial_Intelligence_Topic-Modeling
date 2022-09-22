ceba04e0b594c515016cae8bfd423f4633504f55
Fazal Mahmud Niloy <niloooy@gmail.com>
2022-09-22 23:32:38 +1000

    data partially cleaned

--- main.py
+++ main.py
@@ -5,10 +5,26 @@ import numpy as np
 csv.field_size_limit(1000000000)
 
 # read csv file from data folder
-df = pd.read_csv(os.path.join('data', 'state-of-the-union.csv'), skiprows=0, names=['year','text'])
-# df.columns = ['year', 'text']
-# see first 10 rows
-temp = df.head(1)
-# print 2nd column of temp
-print(temp.iloc[0, 1])
+df = pd.read_csv(os.path.join('data', 'state-of-the-union.csv'), names=['year', 'speech'], skiprows=1)
+
+df['speech'] = df['speech'].str.replace('\nState of the Union Address\n', '')
+df['speech'] = df['speech'].str.replace('\nAddress to Joint Session of Congress \n', '')
+df['speech'] = df['speech'].str.replace('\nAddress on Administration Goals (Budget Message)\n', '')
+df['speech'] = df['speech'].str.replace('\nAddress on Administration Goals\n', '')
+df['speech'] = df['speech'].str.replace('\nAddress to Congress \n', '')
+
+df['president'] = df['speech']
+# delete everything in the president column after the first line break
+df['president'] = df['president'].str.split('\n').str[0]
+df['date'] = df['speech'].str.split('\n').str[1]
+# print president name where president name is empty string
+# print(df[df['president'] == '']['president'])
+# print(df[df['president'] == '']['date'])
+
+temp_date = df[df['date'] == 'Address on Administration Goals (Budget Message)']['speech'].str.split('\n').str[3]
+# print(temp_date)
+df[df['date'] == 'Address on Administration Goals (Budget Message)'] = temp_date.values[0]
+
+print(df['date'].values)
+print(df['president'].values)
 
