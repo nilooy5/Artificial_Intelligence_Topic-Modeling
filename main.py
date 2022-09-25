22373a3b30bb9813a4b75e85f4af666def5fecc6
Fazal Mahmud Niloy <niloooy@gmail.com>
2022-09-25 23:23:01 +1000

    visualization output generated for 15 years

--- main.py
+++ main.py
@@ -193,32 +193,34 @@ from matplotlib import pyplot as plt
 from wordcloud import WordCloud, STOPWORDS
 import matplotlib.colors as mcolors
 
-cols = [color for name, color in mcolors.TABLEAU_COLORS.items()]  # more colors: 'mcolors.XKCD_COLORS'
 
-cloud = WordCloud(stopwords=stop_words,
-                  background_color='white',
-                  width=2500,
-                  height=1800,
-                  max_words=10,
-                  colormap='tab10',
-                  color_func=lambda *args, **kwargs: cols[i],
-                  prefer_horizontal=1.0)
+def visualize_topics(yearly_model):
+    global cols, i
+    cols = [color for name, color in mcolors.TABLEAU_COLORS.items()]  # more colors: 'mcolors.XKCD_COLORS'
+    cloud = WordCloud(stopwords=stop_words,
+                      background_color='white',
+                      width=2500,
+                      height=1800,
+                      max_words=10,
+                      colormap='tab10',
+                      color_func=lambda *args, **kwargs: cols[i],
+                      prefer_horizontal=1.0)
+    topics = yearly_model.show_topics(formatted=False)
+    fig, axes = plt.subplots(1, 4, figsize=(10, 10), sharex=True, sharey=True)
+    for i, ax in enumerate(axes.flatten()):
+        fig.add_subplot(ax)
+        topic_words = dict(topics[i][1])
+        cloud.generate_from_frequencies(topic_words, max_font_size=300)
+        plt.gca().imshow(cloud)
+        plt.gca().set_title('Topic ' + str(i), fontdict=dict(size=16))
+        plt.gca().axis('off')
+    plt.subplots_adjust(wspace=0, hspace=0)
+    plt.axis('off')
+    plt.margins(x=0, y=0)
+    plt.tight_layout()
+    plt.show()
 
-topics = topics_1990.show_topics(formatted=False)
 
-fig, axes = plt.subplots(1, 4, figsize=(10,10), sharex=True, sharey=True)
+for ldaM in yearly_models:
+    visualize_topics(ldaM[0])
 
-for i, ax in enumerate(axes.flatten()):
-    fig.add_subplot(ax)
-    topic_words = dict(topics[i][1])
-    cloud.generate_from_frequencies(topic_words, max_font_size=300)
-    plt.gca().imshow(cloud)
-    plt.gca().set_title('Topic ' + str(i), fontdict=dict(size=16))
-    plt.gca().axis('off')
-
-
-plt.subplots_adjust(wspace=0, hspace=0)
-plt.axis('off')
-plt.margins(x=0, y=0)
-plt.tight_layout()
-plt.show()
